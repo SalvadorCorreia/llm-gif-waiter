23416d12e6b20f10ac6680da87c1efde555f29db
SalvadorCorreia <salvador.sdc.correia@gmail.com>
2026-09-13 22:01:22 +0100

    feat(payload): Make the gif movable

--- payload/index.js
+++ payload/index.js
@@ -1,5 +1,3 @@
-// payload/index.js
-
 window.LLMPayload = {
   container: null,
 
@@ -12,9 +10,40 @@ window.LLMPayload = {
     const img = document.createElement("img");
     img.src = "https://media.giphy.com/media/sIIhZliB2McAo/giphy.gif";
     img.alt = "Nyan Cat";
+    img.draggable = false;
 
     wrapper.appendChild(img);
     this.container.appendChild(wrapper);
+
+    this.makeDraggable(wrapper);
+  },
+
+  makeDraggable: function (element) {
+    element.onmousedown = function (event) {
+      const rect = element.getBoundingClientRect();
+      const shiftX = event.clientX - rect.left;
+      const shiftY = event.clientY - rect.top;
+
+      element.style.transform = "none";
+
+      function moveAt(clientX, clientY) {
+        element.style.left = clientX - shiftX + "px";
+        element.style.top = clientY - shiftY + "px";
+      }
+
+      moveAt(event.clientX, event.clientY);
+
+      function onMouseMove(event) {
+        moveAt(event.clientX, event.clientY);
+      }
+
+      document.addEventListener("mousemove", onMouseMove);
+
+      document.onmouseup = function () {
+        document.removeEventListener("mousemove", onMouseMove);
+        document.onmouseup = null;
+      };
+    };
   },
 
   unmount: function () {
